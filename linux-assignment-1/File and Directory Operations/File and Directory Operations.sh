ca872631b5efe3b331d100201fb4058e2a353c1d
likhon@learnteq.com <likhon@learnteq.com>
2024-11-27 15:01:47 +0530

    File System Navigation and File and Directory Operations

---
+++ linux-assignment-1/File and Directory Operations/File and Directory Operations.sh
@@ -0,0 +1,12 @@
+mkdir linux_fundamentals
+ls
+cd linux_fundamentals/
+mkdir scripts
+ls
+cd ..
+ls
+cd linux_fundamentals/
+ls
+touch example.txt
+cp /home/linux_fundamentals/example.txt /home/linux_fundamentals/scripts/
+mv /home/linux_fundamentals/example.txt /home/linux_fundamentals/backup/exmaple.txt
---
+++ linux-assignment-1/File_System_Navigation/File_System_Navigation.sh
@@ -0,0 +1,9 @@
+#./bin/bash
+
+cd home
+ls
+cd ..
+cd var
+cd log
+which bash
+echo $SHELL
